0371ac8aff8042975bb08b01272dd9b8295e0475
Oleksandr Khatskalev <oleksandrkhatskalev@Oleksandrs-MacBook-Air.local>
2026-01-20 11:52:51 +0100

    polish 7.reverse integer

--- Array/7.Reverse_Integer/solution.ts
+++ Array/7.Reverse_Integer/solution.ts
@@ -2,21 +2,18 @@ const INT_MAX = 2147483647;
 const INT_MIN = -2147483648;
 
 function reverse(x: number): number {
-  if (x === 0) return x;
-
   let res = 0;
-  const sign = Math.sign(x);
-  let currentValue = Math.abs(x);
+  let curr = Math.abs(x);
 
-  while (currentValue > 0) {
-    const num = currentValue % 10;
+  while (curr > 0) {
+    const num = curr % 10;
     res = res * 10 + num;
-    currentValue = Math.floor(currentValue / 10);
+    curr = Math.floor(curr / 10);
   }
 
-  const returnValue = res * sign;
-  if (returnValue > INT_MAX || returnValue < INT_MIN) return 0;
-  return returnValue;
+  res *= Math.sign(x);
+  if (res > INT_MAX || res < INT_MIN) return 0;
+  return res;
 };
 
 const reverseRes = reverse(-123);
