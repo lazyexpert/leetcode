36a9970e46804827fe898a23d0cc149ca64ba76b
Oleksandr Khatskalev <oleksandrkhatskalev@Oleksandrs-MacBook-Air.local>
2026-01-20 10:57:27 +0100

    Array/zigzag

---
+++ Array/6.Zigzag_Conversion/solution.ts
@@ -0,0 +1,26 @@
+
+function convert(s: string, numRows: number): string {
+  if (s.length === 1 || numRows > s.length || numRows === 1) return s;
+  const zigzag = new Array(numRows);
+  for (let i = 0; i < numRows; i++) {
+    zigzag[i] = "";
+  }
+
+  let direction = 1; // 1 - down, -1 - up
+  let currentIndex = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const currString: string = zigzag[currentIndex];
+    zigzag[currentIndex] = currString + s[i];
+
+    if (currentIndex === numRows - 1) direction = -1;
+    else if(currentIndex === 0) direction = 1;
+
+    currentIndex += direction;
+  }
+
+  return zigzag.join("");
+};
+
+const convertRes = convert("PAYPALISHIRING", 3);
+console.log(convertRes);
--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 [2. Add Two Numbers](Array/2.Add_Two_Numbers)
 [3. Longest Substring Without Repeating Characters](Array/3.Longest_Substring_Without_Repeating_Characters)
 [5. Longest Palindromic Substring](Array/5.Longest_Palindromic_Substring)
+[6. Zigzag Conversion](Array/6.Zigzag_Conversion)
 [20. Valid Parentheses](String/20.Valid_Parentheses/)
 [66. Plus One](Math/66.Plus_One/)
 [155. Min Stack](Stack/155.Min_Stack/)
